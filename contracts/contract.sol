1d13ad62f6d6d22b1c7a2aaeb6c79633b1d94ef5
Programmer18 <febrymuhammad80@gmail.com>
2025-09-09 13:12:56 +0700

    feat: update contract

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -2,29 +2,41 @@
 pragma solidity ^0.8.0;
 
 contract OnchainRaffle {
-    // model
+    constructor() {
+        owner = msg.sender;
+    }
+
+    enum RaffleStatus {
+        ACTIVE,
+        INACTIVE,
+        STARTED
+    }
+
+    // models
     struct Raffle {
         address creator;
         uint256 balance;
-        bool active;
+        RaffleStatus status;
         uint256 maxParticipant;
         uint256 minParticipant;
         uint256 totalParticipant;
     }
 
     uint256 public nextRaffleId;
+    address public owner;
 
     mapping(uint256 => Raffle) public raffles;
     mapping(uint256 => mapping(address => bool)) private hasJoined;
-    mapping(uint256 => address[]) public participants;
+    mapping(uint256 => address[]) private participants;
     mapping(uint256 => address) public winners;
 
-    // event
+    // events
     event RaffleCreated(
         uint256 indexed id,
         address indexed creator,
         uint256 initialDeposit
     );
+    event RaffleStarted(uint256 indexed id);
     event WinnerSelected(
         uint256 indexed id,
         address indexed user,
@@ -44,11 +56,12 @@ contract OnchainRaffle {
     );
     event RaffleClosed(uint256 indexed id);
 
-    // main function
-    function createRaffle(
-        uint256 maxParticipant,
-        uint256 minParticipant
-    ) external payable returns (uint256) {
+    // main functions
+    function createRaffle(uint256 maxParticipant, uint256 minParticipant)
+        external
+        payable
+        returns (uint256)
+    {
         require(msg.value > 0, "Initial deposit is required");
         require(
             maxParticipant > minParticipant,
@@ -58,18 +71,23 @@ contract OnchainRaffle {
         raffles[nextRaffleId] = Raffle(
             msg.sender,
             msg.value,
-            true,
+            RaffleStatus.ACTIVE,
             maxParticipant,
             minParticipant,
             0
         );
+
         emit RaffleCreated(nextRaffleId, msg.sender, msg.value);
         return nextRaffleId++;
     }
 
     function deposit(uint256 raffleId) external payable {
         Raffle storage raffle = raffles[raffleId];
-        require(raffle.active, "Raffle is already inactive");
+        require(_isRaffleExist(raffleId), "Raffle is not exist");
+        require(
+            raffle.status == RaffleStatus.ACTIVE,
+            "Raffle is already inactive or started"
+        );
         require(msg.value > 0, "Deposit must be greater than 0");
 
         raffle.balance += msg.value;
@@ -77,23 +95,54 @@ contract OnchainRaffle {
         emit Deposited(raffleId, msg.sender, msg.value);
     }
 
-    function startRaffle(uint256 raffleId, address user) external {
+    function startRaffle(uint256 raffleId) external {
         Raffle storage raffle = raffles[raffleId];
-        require(raffle.active, "Raffle is already inactive");
+        require(
+            raffle.status == RaffleStatus.ACTIVE,
+            "Raffle is already inactive or started"
+        );
 
-        (bool ok, ) = payable(user).call{value: raffle.balance}("");
-        require(ok, "Transfer failed");
+        raffle.status = RaffleStatus.STARTED;
 
-        emit WinnerSelected(raffleId, user, raffle.balance);
+        emit RaffleStarted(raffleId);
+    }
+
+    function winnerSelected(uint256 raffleId, address winner) external {
+        Raffle storage raffle = raffles[raffleId];
+        require(raffle.status == RaffleStatus.STARTED, "Raffle is not started");
+
+        winners[raffleId] = winner;
+        raffle.status = RaffleStatus.INACTIVE;
+
+        (bool ok, ) = payable(winner).call{value: raffle.balance}("");
+        require(ok, "Winner payout failure");
+
+        emit WinnerSelected(raffleId, winner, raffle.balance);
+    }
+
+    function raffleParticipants(uint256 raffleId)
+        external
+        view
+        returns (address[] memory)
+    {
+        return participants[raffleId];
     }
 
     function joinRaffle(uint256 raffleId) external {
         Raffle storage raffle = raffles[raffleId];
-        require(raffle.active, "Raffle is already inactive");
+        require(
+            raffle.creator != msg.sender,
+            "You can't join to your own raffle"
+        );
+        require(
+            raffle.status == RaffleStatus.ACTIVE,
+            "Raffle is already inactive or started"
+        );
         require(
             raffle.totalParticipant < raffle.maxParticipant,
             "Raffle is full"
         );
+
         bool joined = hasJoined[raffleId][msg.sender];
         require(!joined, "You have already joined");
 
@@ -106,7 +155,10 @@ contract OnchainRaffle {
 
     function leaveRaffle(uint256 raffleId) external {
         Raffle storage raffle = raffles[raffleId];
-        require(raffle.active, "Raffle is already inactive");
+        require(
+            raffle.status == RaffleStatus.ACTIVE,
+            "Raffle is already inactive or started"
+        );
         bool joined = hasJoined[raffleId][msg.sender];
         require(joined, "You have not joined the raffle yet");
 
@@ -127,7 +179,10 @@ contract OnchainRaffle {
 
     function claimReward(uint256 raffleId) external {
         Raffle storage raffle = raffles[raffleId];
-        require(raffle.active, "Raffle is already inactive");
+        require(
+            raffle.status == RaffleStatus.STARTED,
+            "Raffle is not started yet"
+        );
 
         emit Claimed(raffleId, msg.sender, 0);
     }
@@ -136,9 +191,13 @@ contract OnchainRaffle {
         Raffle storage raffle = raffles[raffleId];
         require(raffle.creator == msg.sender, "You are not the host");
 
-        raffle.active = false;
+        raffle.status = RaffleStatus.INACTIVE;
 
         emit RaffleClosed(raffleId);
+    }
+
+    function _isRaffleExist(uint256 raffleId) private view returns (bool) {
+        return raffles[raffleId].creator != address(0);
     }
 
     receive() external payable {
